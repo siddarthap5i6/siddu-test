be5fa5c073cbcb0656798b52e1ba77da57372055
siddarthap5i6 <102055302+siddarthap5i6@users.noreply.github.com>
2022-03-21 14:14:52 -0500

    New

---
+++ mymodule.py
@@ -0,0 +1,23 @@
+def greeting(name):
+  print("Hello, " + name)
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
+person2 = {
+  "name": "kim ",
+  "age": 34,
+  "country": "usa"
+}
+person3 = {
+  "name": "luci",
+  "age": 22,
+  "country": "canada"
+}
+person4 = {
+  "name": "lucifer",
+  "age": 21,
+  "country": "UK"
+}
